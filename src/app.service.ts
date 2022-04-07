5064c2dca569cb104977ccedf784fbe38c9847e6
Juan Camilo <elecjuanca@utp.edu.co>
2022-04-07 13:00:17 -0500

    Implementation of other environment using joi validation and Config package.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+
+import config from './config';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
   getHello(): string {
-    return '[C10-Squad-Search-Engine]💚 Platzi Master';
+    const environment = this.configService.environment;
+    return `[C10-Squad-Search-Engine]💚 Platzi Master : ${environment} Environment`;
   }
 }
